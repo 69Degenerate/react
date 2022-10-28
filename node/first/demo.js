657295391ac73558c059cad1d76577af59fe6ae4
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-28 22:41:43 +0530

    using gitignore first time

---
+++ node/first/demo.js
@@ -0,0 +1,8 @@
+module.exports={
+sa:'sads',
+no:0,
+myFunction() {
+    document.getElementById("no").innerHTML = "I have changed!";
+    document.getElementById("demo").innerHTML = "I have changed!";
+  }
+  }
---
+++ node/first/index.js
@@ -0,0 +1,5 @@
+const imp=require('./demo');
+let sad='sad motherfucker';
+console.log(sad);
+console.log(process.env.USER);
+console.log(imp.sa);
